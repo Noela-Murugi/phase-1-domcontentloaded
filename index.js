fa3426650d671034bf96d50e9ed003c145a1a1be
Noela-Murugi <murugin254@gmail.com>
2022-08-21 21:13:12 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    document.getElementById("text").innerHTML = "This is really cool!";
+    // document.getElementById("text").textContent = "This is really cool!";
+
+  });
+
+  console.log("Before the DOM loads");
